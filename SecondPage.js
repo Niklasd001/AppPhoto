eaa5da5d5eb04d15fc8f04ceaefe224eeda37eca
niklas01 <nicholasdm23@gmail.com>
2023-07-04 17:22:55 +0200

    prova commit

--- SecondPage.js
+++ SecondPage.js
@@ -2,16 +2,62 @@ import React from 'react';
 import { View, Text, Button , Image, StyleSheet} from 'react-native';
 import { Camera } from 'expo-camera';
 import { NavigationContainer } from '@react-navigation/native';
+import { OpenCvProvider, useOpenCv } from 'opencv-react'
 
+const cv = require()
 
-export default function SecondPage({route}) {
-  const photoUri = route.params;
+// Pagina realmente visualizzabile
+function RealPage() {
+
+  // Load openCV
+  const { loaded, cv } = useOpenCv()
+
+  useEffect(() => {
+
+    if (cv) {
+
+      // Usare opencv
+
+    }
+
+  }, [cv])
+  
+  return (
+          
+    <View>
+
+      <Text>GALLERIA</Text>
+      <Image source={{ uri: photoUri }} style={styles.image} />
+    
+    </View>
+  )
+}
+
+// Componente visibile esternamente 
+function SecondPage({route}) {
+
+    // Uri immagine scattata
+    const photoUri = route.params;
+
+    // Serve per capire se openCV è stato caricato correttamente
+    const onLoaded = (cv) => {
+
+      console.log('opencv loaded', cv);
+
+    }
+
     return (
-      <View>
-        <Text>GALLERIA</Text>
-        <Image source={{ uri: photoUri }} style={styles.image} />
-      </View>
+
+        // Provide OpenCV
+        <OpenCvProvider onLoad={onLoaded}>
+
+          {/* Pagine vera */}
+          <RealPage />
+
+        </OpenCvProvider>
+
     );
+
   } 
 
   const styles = StyleSheet.create({
@@ -26,3 +72,4 @@ export default function SecondPage({route}) {
     },
   });
  
+export default SecondPage;
